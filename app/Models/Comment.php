fb4d0b786dd5e0ef7a387fbce5aa6fd6f8aadbf6
TresorKL <trezmathieu@gmail.com>
2023-02-21 03:46:25 +0200

    set up remote db

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+//hjkd
 class Comment extends Model
 {
     protected $fillable=[ 'commented_by','comment_text','post_id' ];

b3791397d87e0708d1063aa557039579409c87a6
TresorKL <trezmathieu@gmail.com>
2023-02-21 14:29:59 +0200

    as img name

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
 
         $imageName=$request->file('image')->getClientOriginalName();
         $image=$current_date_time.$id.$imageName;
-        $request->file('image')->store('/images/','s3');
+        $request->file('image')->storeas('/images/',$image,'s3');
       //$file=$request->file('image');
       // Storage::disk('s3')->put( $image, file_get_contents( $file));
         $user->post()->create([
